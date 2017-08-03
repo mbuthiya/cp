0be5f69958e1ff8bbbb9d27056d6296095116730
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-08-03 16:08:01 +0300

    Add index.html and render route

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 """
 Defines all the routes in the application and creates view functions that handle request from routes
 """
+from flask import render_template
 from app import app
 
 @app.route('/')
@@ -9,4 +10,4 @@ def index():
     """
     Handler for the index route.
     """
-    return '<h1> Hello World </h1>'
+    return render_template('index.html')
