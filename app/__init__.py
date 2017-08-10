7745132f3cd203444b365e2462d7fa1639b53ca8
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-08-10 13:25:21 +0300

    Import Bootstrap class
    Imported the bootstrap class and instanciated it

--- app/__init__.py
+++ app/__init__.py
@@ -2,5 +2,10 @@
 Creates the application Instance from the Flask class and instanciates all other flask extensions.
 """
 from flask import Flask
+from flask_bootstrap import Bootstrap
+
+# Initializations
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
 from app import views
