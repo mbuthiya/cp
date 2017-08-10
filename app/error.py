a38c47ef2d819e764a99dafa0f25ac14f30a7fe4
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-08-10 17:01:38 +0300

    Added error handler for 404

--- app/error.py
+++ app/error.py
@@ -1,2 +1,9 @@
 from flask import render_template
 from app import app
+
+@app.errorhandler(404)
+def page_not_found(e):
+    '''
+    Returns a custom error page for any 400 server errors encountered
+    '''
+    return render_template('404.html'),404
