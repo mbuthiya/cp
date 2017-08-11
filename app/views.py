0911dda075914814375e81dd09f0668f0bb58129
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-08-11 11:16:00 +0300

    Added view functions for the navbar urls

--- app/views.py
+++ app/views.py
@@ -16,3 +16,51 @@ def index():
     scandal_blogs = ['Did Jk Rowling do it?', 'The Oj Simpson Story','Mother of dragons fryer of eggs']
 
     return render_template('index.html',title = "Home",popular_blogs = popular_blogs,scandal_blogs = scandal_blogs)
+
+
+@app.route('/movies')
+def movies():
+    '''
+    View function that returns movie blogs
+    '''
+    title ='movies'
+    return render_template('movies.html' title =title)
+
+
+
+@app.route('/shows')
+def shows():
+    '''
+    View function that returns tvshow blogs
+    '''
+    title ='shows'
+    return render_template('shows.html' title =title)
+
+
+
+@app.route('/news')
+def news():
+    '''
+    View function that returns news blogs
+    '''
+
+    title ='news'
+    return render_template('news.html' title =title)
+
+
+@app.route('/login')
+def login():
+    '''
+    View function that returns a login form
+    '''
+    title = 'login'
+    return render_template('login.html' title =title)
+
+
+@app.route('/signup')
+def signup():
+    '''
+    View function that returns a signup form
+    '''
+    title ='signup'
+    return render_template('signup.html' title =title)
